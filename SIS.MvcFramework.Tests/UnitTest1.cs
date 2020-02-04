42376b09b27aeefc5ffe6576d78b79683a68ff0d
Nikolay Doichev <nikolay.doichev@gmail.com>
2020-02-04 22:49:12 +0200

    View Engine

---
+++ SIS.MvcFramework.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace SIS.MvcFramework.Tests
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData("")]
+        public void Test1(string testName)
+        {
+
+        }       
+        
+    }
+}
